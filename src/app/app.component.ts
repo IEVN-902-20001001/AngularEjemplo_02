0c07545bfe2272885dce80c604a718aa6d869e9c
Miguel Hernandez <20001001@alumnos.utleon.edu.mx>
2023-09-19 23:23:04 -0600

    19-09-2023 MHN

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,16 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'AngularEjemplo_02';
-}
+
+export class AppComponent{
+  title='AngularEjemplo_02'
+   duplicarNumero(n:number):number{
+       return n+2;
+   }
+ 
+   pelicula={
+       titulo:'Cantinflas show',
+       fechadeLanzamiento:new Date(),
+       precio:1234
+   }
+ }
