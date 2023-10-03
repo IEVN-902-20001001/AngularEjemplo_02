8373d391e331246ef88a0b30ce24f524429b8ea3
Miguel Hernandez <20001001@alumnos.utleon.edu.mx>
2023-10-03 17:42:08 -0600

    03-10-2023 MHN

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
-import{MatInputModule} from '@angular/material/input';
-import{MatButtonModule} from '@angular/material/button';
-import{MatToolbarModule} from '@angular/material/toolbar';
-import{MatIconModule} from '@angular/material/icon';
+
 
 
 import { AppComponent } from './app.component';
@@ -15,6 +12,7 @@ import { OperasBasComponent } from './formularios/operas-bas/operas-bas.componen
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { Suma2Component } from './formularios/suma2/suma2.component';
 import { MenuComponent } from './menu/menu/menu.component';
+import { MaterialModule } from './material/material/material.module';
 
 @NgModule({
   declarations: [
@@ -29,10 +27,7 @@ import { MenuComponent } from './menu/menu/menu.component';
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
-    MatInputModule,
-    MatButtonModule,
-    MatIconModule,
-    MatToolbarModule
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
